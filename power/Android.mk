f660aed2df2928aef3777c92e0280c336db49818
Pietro Ivan Cerrato <ivan@cerrato.biz>
2019-08-28 19:20:03 +0200

    overlay: final configuration

--- power/Android.mk
+++ power/Android.mk
@@ -8,7 +8,6 @@ endif
 
 LOCAL_MODULE := power.qcom
 LOCAL_INIT_RC := android.hardware.power@1.0-service.rc
-LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
