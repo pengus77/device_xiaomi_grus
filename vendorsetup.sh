36d98a77450ec59e015a324a93868d75f75022fe
Pietro Ivan Cerrato <ivan@cerrato.biz>
2019-10-01 15:26:19 +0200

    patches: disable'em'all

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,3 @@
-# apply patches
-workdir=$(pwd)
-cd hardware/qcom/audio-caf/sdm845 || return
-git reset -q --hard
-[ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/libtinycompress.patch
-cd "$workdir" || return
-
-# end patches
-
 
 add_lunch_combo lineage_grus-userdebug
 
