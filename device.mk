4b4bf012dee0da4a5142bbd27e3eb59ca36f3587
pengus77 <ivan@cerrato.biz>
2020-07-11 18:18:57 +0200

    bt: build bt-configstore@1.0 now

--- device.mk
+++ device.mk
@@ -523,7 +523,8 @@ PRODUCT_PACKAGES += \
     libldacBT_enc \
     libldacBT_abr \
     hwaddrs \
-    libbt-vendor
+    libbt-vendor \
+    vendor.qti.hardware.btconfigstore@1.0
 
 # qti telephony
 PRODUCT_PACKAGES += \
