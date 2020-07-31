a5c815dd0820a9dfc0efebcce97e783248936d37
pengus77 <ivan@cerrato.biz>
2020-07-31 20:05:35 +0200

    bt: move btconfigstore hidl to vendor

--- device.mk
+++ device.mk
@@ -525,7 +525,7 @@ PRODUCT_PACKAGES += \
     hwaddrs \
     libbt-vendor \
     libbtconfigstore \
-    vendor.qti.hardware.btconfigstore@1.0
+    vendor.qti.hardware.btconfigstore@1.0.vendor 
 
 # qti telephony
 PRODUCT_PACKAGES += \
