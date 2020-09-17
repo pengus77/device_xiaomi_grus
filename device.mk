4c31ca40aa974205c58a3b14bdec6b528c232fdb
pengus77 <ivan@cerrato.biz>
2020-09-17 12:19:01 +0200

    audio: don't build audio.primary and sound_trigger. use prebuilts

--- device.mk
+++ device.mk
@@ -109,8 +109,6 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    sound_trigger.primary.sdm710 \
-    audio.primary.sdm710 \
     libaudioroute \
     libhdmiedid \
     libhfp \
