17db91bfbad57dca59be1c4816e2059d562fca7e
Pietro Ivan Cerrato <ivan@cerrato.biz>
2020-09-30 14:02:25 +0200

    interfaces: rework directory structure and cleanup

---
+++ interfaces/goodix/update-makefiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+source /android/cesium/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.goodix:device/xiaomi/grus/interfaces/goodix" \
