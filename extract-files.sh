517082855d1aaf9f71a2ba26644b9a16edcad1dc
LuK1337 <priv.luk@gmail.com>
2020-10-22 11:56:22 +0200

    grus: Use libcutils-v29.so for libdpmframework.so (1/2)
    * v30 one doesn't have `strdup8to16` symbol in it.

--- extract-files.sh
+++ extract-files.sh
@@ -34,6 +34,19 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+    product/lib/libdpmframework.so)
+        patchelf --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
+        patchelf --add-needed "libcutils.so" "${2}"
+        ;;
+    product/lib64/libdpmframework.so)
+        patchelf --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
+        patchelf --add-needed "libcutils.so" "${2}"
+        ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=false
 SECTION=
